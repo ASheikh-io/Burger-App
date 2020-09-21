805d867557c5dceb5aac99b4483b99159eb9f6ce
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-09-20 21:12:58 -0400

    created seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers_name (name) VALUES ('Cheeseburger');
+INSERT INTO burgers_name (name) VALUES ('Quarter Pounder');
+INSERT INTO burgers_name (name, devoured) VALUES ('Triple Meat Deluxe', true);
+INSERT INTO burgers_name (name, devoured) VALUES ('Chicken Sliders', true);
+INSERT INTO burgers_name (name, devoured) VALUES ('Ruben', true);
+INSERT INTO burgers_name (name) VALUES ('Owens');
