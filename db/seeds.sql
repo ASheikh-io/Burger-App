d6e5571bae11fc4165028c492930b70fdf212a8f
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-12-01 17:25:38 -0500

    added seeds to database and created partials

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burger_name (name) VALUES ('Cheeseburger');
-INSERT INTO burger_name (name) VALUES ('Quarter Pounder');
-INSERT INTO burger_name (name, devoured) VALUES ('Triple Meat Deluxe', true);
-INSERT INTO burger_name (name, devoured) VALUES ('Chicken Sliders', true);
-INSERT INTO burger_name (name, devoured) VALUES ('Ruben', true);
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Meat Deluxe', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Sliders', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ruben', true);
 
