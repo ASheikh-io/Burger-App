3e86613a28692ee0c1d8617f0bdc59c6e1832b1f
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-12-01 02:20:07 -0500

    controller for the routes set up

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,7 +4,69 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", async function (req, res) {
+    const hbsObject = {
+        burgers: await burger.all()
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+});
 
+router.post("/api/burgers", async function (req, res) {
+    try {
+        const result = await burger.create(
+            [
+                "name", "devoured"
+            ],
+            [
+                req.body.name, req.body.devoured
+            ]
+        );
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+router.put("/api/burgers/:id", async function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    try {
+        const result = await burger.update({
+            devoured: req.body.devoured
+        }, condition);
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+
+router.delete("/api/burgers/:id", async function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    try {
+        const result = await burger.delete(condition)
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+// Export routes for server.js to use.
+module.exports = router;
 
 
 
