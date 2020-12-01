2b440171d2d4510ad8b111be2b459354923dd91f
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-12-01 16:04:36 -0500

    created handlebar views

--- config/orm.js
+++ config/orm.js
@@ -1,44 +1,100 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-class ORM {
-    connection;
-
-    constructor(connection) {
-        this.connection = connection;
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
     }
-
-    query = (queryString, vals) => {
-        return new Promise((resolve, reject) => {
-            this.connection.query(queryString, vals, function (err, result) {
-                if (err) {
-                    reject(err);
-                }
-                resolve(result);
-            });
-        })
-    };
-    // Helper function for SQL syntax.
-    // Let's say we want to pass 3 values into the mySQL query.
-    // In order to write the query, we need 3 question marks.
-    // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-    // ["?", "?", "?"].toString() => "?,?,?";
-
-    selectAll() {
-        return this.query("SELECT * FROM burgers");
-    };
-
-    insertOne(burgerName) {
-        return this.query(`INSERT INTO burgers (burger_name, devoured) VALUES ('${burgerName}', false),`);
+  
+    return arr.toString();
+  }
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+          
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        
+        arr.push(key + "=" + value);
+      }
     }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  
+  // Object for all our SQL statement functions.
+  var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
 
-    updateOne(id) {
-        return this.query(`UPDATE burgers SET devoured = 1 WHERE id = ${id}`)
+    update: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+    delete: function(table, condition, cb) {
+      var queryString = "DELETE FROM " + table;
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
     }
-    deleteOne() {
-        return this.query(`DELETE FROM burgers WHERE id = ${id}`)
-    };
-};
-
-// Export the promise orm object.
-module.exports = new ORM(connection);
+  };
+  
+  module.exports = orm;
