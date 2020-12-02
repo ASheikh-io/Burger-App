100a044519cbff89e8f8b543eacb25991e171446
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-12-02 01:31:57 -0500

    updates style and views

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -19,40 +19,43 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.create("burger_name", req.body.burger, function (result) {
-        res.json({ id: result.insertId });
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
     });
-});
-
-router.put("/api/burgers/:id", function (req, res) {
-    
+  });
+  
+  router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-    var devoured = "devoured=" + req.body.devoured;
-    
-    burger.update(devoured, condition, function (result) {
-
-     
-
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        }
-        else {
-            res.status(200).end();
-        }
-
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function (result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     });
-});
-
-router.delete("/api/burgers/:id", function (req, res) {
+  });
+  
+  router.delete("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
+  
     burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     });
-});
-
-module.exports = router;
+  });
+  
+  // Export routes for server.js to use
+  module.exports = router;
